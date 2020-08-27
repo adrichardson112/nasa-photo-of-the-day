b5c5d15376c320ae3e1765a89d866e4ef111e981
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-08-27 19:54:08 -0400

    adding css

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -5,9 +5,11 @@ function PhotoCard (props) {
         <div className="photoCard">
             <h2>{props.title}</h2>
             <img src={props.url}></img>
+            <div className="written">
             <p>Date: {props.date}</p>
             <p>{props.explanation}</p>
         </div>
+    </div>
     );
 };
 
